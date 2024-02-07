6194a235e1107720cc8b69e641dfec9d5a471e5b
nimbus2000 <nimbus2000@victus.com>
2024-02-07 23:01:27 +0100

    Ajout auth

--- user/main.c
+++ user/main.c
@@ -16,82 +16,8 @@
 #include <sys/types.h>
 #include <netdb.h>
 #include <arpa/inet.h>
-/*
-int get_buff_array(char * buf[10], char * cmd)
-{
-	// ten bloc max
-	char bloc_str[64]; // command size
-	int i = 0;
-	int j = 0;
-	int k = 0;
-	while(1)
-	{
-		char c = cmd[k];
-		bloc_str[i] = c;
-		i++;
-		if(c == ' ' || c == '\n')
-		{
-			buf[j] = malloc(sizeof(char)*i);
-			strncpy(buf[j], bloc_str, i);
-			memset(bloc_str, '\0', sizeof(char)*64);	
-			j++;
-			i = 0;
-		}
-		if(c == '\n')
-		{
-			break;
-		}
-		k++;
-	}
-	return j;
-}
 
-int exec_cmd(char buf[1024], server_ftp_t * server_ftp, client_ftp_t * client_ftp)
-{
-	char * buff_array[10];
-	int nb_bloc = get_buff_array(buff_array, buf);
-	if(strncmp(buff_array[0],"cmd", 3) == 0)
-	{
-		if(strncmp(buff_array[1], "auth", 4) == 0)
-		{
-			if(nb_bloc == 4)
-			{
-				server_ftp->auth(buff_array[2],buff_array[3]);
-			} 
-			else
-			{
-				printf("auth take 2 arguments: login password\n");	
-			}
-		} 
-	}
-	if(strncmp(buff_array[0], "lst", 3) == 0)
-	{
-		if(strncmp(buff_array[1], "auth", 4) == 0)
-		{
-			if(nb_bloc == 3)
-			{
-				server_ftp->update_metadata();
-			}
-		}
-	}
-	return 0;
-}
-
-int command(int p_cmd)
-{
-	while(1)
-	{
-		char buffer[32];
-    		char *b = buffer;
-    		size_t bufsize = 32;
-    		size_t characters;
-		characters = getline(&b,&bufsize,stdin);
-		char buffer2[32] = "cmd ";
-		strcat(buffer2, buffer);
-		write(p_cmd, buffer2, 32);
-	}
-}
-*/
+#define MAX_BUFFER_SIZE 256
 
 void read_stdin(char * str_ptr, int len)
 {
@@ -110,6 +36,68 @@ void read_stdin(char * str_ptr, int len)
 	}
 }
 
+
+void saisirLoginPassword(char *login, char *password, char * buffer) 
+{
+	printf("Entrez votre login : ");
+	fgets(login, MAX_BUFFER_SIZE, stdin);
+	login[strcspn(login, "\n")] = '\0';  // Supprime le caractère de nouvelle ligne
+
+	printf("Entrez votre mot de passe : ");
+	fgets(password, MAX_BUFFER_SIZE, stdin);
+	password[strcspn(password, "\n")] = '\0';  // Supprime le caractère de nouvelle ligne
+	
+	// Concaténer login et password dans buffer
+    	snprintf(buffer, MAX_BUFFER_SIZE * 2, "%s,%s", login, password);
+}
+
+void attend_reponse(char * adresseIP, char * port)
+{
+    int sockfd = -1;
+    sockfd = listener(adresseIP, port, 0);
+    listen(sockfd, 10);
+    struct sockaddr_in serveurAddr, clientAddr;
+    socklen_t clientAddrLen = sizeof(clientAddr);
+
+
+    // Attente de la connexion d'un client
+    int clientSocket = accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrLen);
+    if (clientSocket == -1) {
+        perror("Erreur lors de l'acceptation de la connexion du client");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connexion acceptée depuis %s:%d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+
+    // Lecture du message du client
+    char buffer[MAX_BUFFER_SIZE];
+    ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesRead == -1) {
+        perror("Erreur lors de la lecture du message du client");
+    } else if (bytesRead == 0) {
+        printf("La connexion du client est fermée.\n");
+    } else {
+        // Affichage du message reçu
+        buffer[bytesRead] = '\0';
+        printf("Message du client : %s\n", buffer);
+
+	if (buffer[0] == 'A') {
+            printf("Connexion accepté...\n");
+         } 
+	 else{
+            printf("Connexion non accepté.\n");
+            close(clientSocket);
+            close(sockfd);
+            exit(EXIT_SUCCESS);
+	 }
+    }
+
+    // Fermeture des sockets
+    close(clientSocket);
+    close(sockfd);	
+}
+
 int main(int argc, char * argv[])
 {
 	if(argc != 5)
@@ -117,6 +105,22 @@ int main(int argc, char * argv[])
 		fprintf(stderr, "Nombre d'arguments non valide:\n- Arg1: adresse hostname local\n- Arg2: port local\n- Arg3: adresse hostname serveur centrale distant\n- Arg4: port distant\n");
 		exit(EXIT_FAILURE);
 	}
+	
+	printf("Authentification\n");
+	char login[MAX_BUFFER_SIZE];
+	char password[MAX_BUFFER_SIZE];
+	char buffer[MAX_BUFFER_SIZE * 2];
+
+	saisirLoginPassword(login, password,buffer);
+
+	// Vous pouvez maintenant utiliser les valeurs de login et password comme nécessaire.
+
+	printf("Login saisi : %s\n", login);
+	printf("Mot de passe saisi : %s\n", password);
+
+	talker(argv[3],argv[4],buffer);
+	
+	attend_reponse(argv[1],argv[2]);	
 
 	printf("Liaison du port hostname de reception %s:%s\n", argv[1],argv[2]);	
 	printf("Démmarage du serveur centrale...\n");
@@ -176,16 +180,7 @@ int main(int argc, char * argv[])
 	}
 
 	printf("Initialisation du serveur_ftp...\n");
-	/*	
-	char inbuf[1024];  	
-	int nbytes;
-	while ((nbytes = read(p_server[0], inbuf, 1024)) > 0) 
-            printf("% s\n", inbuf); 
-        if (nbytes != 0) 
-            exit(2); 
-        printf("Finished reading\n"); 	
-	*/
-
+	
 	// Initialisation du serveur_ftp
 	server_ftp_t * server_ftp;	
 	server_ftp = malloc(sizeof(server_ftp_t));
@@ -239,65 +234,5 @@ int main(int argc, char * argv[])
 		read_stdin(key_ptr, 32);
 		client_ftp->search_key(client_ftp, key);
 	}
-	/*
-	// Init server_ftp
-	server_ftp_t * server_ftp;
-        server_ftp = malloc(sizeof(server_ftp_t));
-	// si null
-	// server_ftp->sockfd = -1;
-
-	// Init client_ftp	
-	client_ftp_t * client_ftp;
-	client_ftp = malloc(sizeof(client_ftp_t));
-	// si null
-	
-	int p[2]; // pipe 2 files descriptor for cmd		
-	pipe(p);
-	
-	int pid_cmd = fork();
-	if(pid_cmd == -1)
-	{
-		fprintf(stderr, "erreur de la création du fork command");
-		exit(EXIT_FAILURE);
-	}
-	if(pid_cmd == 0)
-	{
-		printf("$> Listening in cmd\n");
-		
-		command(p[1]);
-		
-		close(p[1]);
-		exit(0);
-	}
-	if(pid_cmd > 0)
-	{
-		// void	
-	}
-	
-	//int p_lst[2]; // pipe 2 files descriptor for listener
-	// pipe(p_lst);
-	
-	int pid_lst = fork();
-	
-	if(pid_lst == -1)
-	{
-		fprintf(stdout, "erreur fork\n");
-		return -1;
-	}
-	if(pid_lst == 0)
-	{
-		printf("$> Listening in : %s:%s\n", argv[1],argv[2]); 
-		listener(argv[1], argv[2], p[1]);	
-		exit(0);
-	}
-	if(pid_lst > 0)
-	{
-		char buf[1024];
-		while(read(p[0],buf,1024)>0)
-		{ 
-			exec_cmd(buf, server_ftp, client_ftp);
-		}
-	}
-	*/	
 	return 0;
 }
